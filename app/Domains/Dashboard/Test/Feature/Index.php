c2cfa2880d38c6e5cd99691ea76b72b46eab25c4
Lito <lito@eordes.com>
2021-09-15 22:57:24 +0200

    Added Dashboard and PWA tests

---
+++ app/Domains/Dashboard/Test/Feature/Index.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace App\Domains\Dashboard\Test\Feature;
+
+class Index extends FeatureAbstract
+{
+    /**
+     * @var string
+     */
+    protected string $route = 'dashboard.index';
+
+    /**
+     * @return void
+     */
+    public function testGetUnauthorizedFail(): void
+    {
+        $this->get($this->route())
+            ->assertStatus(302)
+            ->assertRedirect(route('user.auth.credentials'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testPostUnauthorizedFail(): void
+    {
+        $this->post($this->route())
+            ->assertStatus(405);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetMineSuccess(): void
+    {
+        $this->authUser();
+
+        $this->get($this->route())
+            ->assertStatus(302)
+            ->assertRedirect(route('app.index'));
+    }
+}
---
+++ app/Domains/PWA/Test/Feature/Index.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace App\Domains\PWA\Test\Feature;
+
+class Index extends FeatureAbstract
+{
+    /**
+     * @var string
+     */
+    protected string $route = 'pwa.index';
+
+    /**
+     * @return void
+     */
+    public function testGetUnauthorizedFail(): void
+    {
+        $this->get($this->route())
+            ->assertStatus(200)
+            ->assertViewIs('domains.pwa.index');
+    }
+
+    /**
+     * @return void
+     */
+    public function testPostUnauthorizedFail(): void
+    {
+        $this->post($this->route())
+            ->assertStatus(405);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetMineSuccess(): void
+    {
+        $this->authUser();
+
+        $this->get($this->route())
+            ->assertStatus(200)
+            ->assertViewIs('domains.pwa.index');
+    }
+}
