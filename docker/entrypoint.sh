3dd2e0c91f0026270e3acd3779dcf402a1e12f4a
Henry Glueck <henry.glueck@allegro-packets.com>
2022-08-24 10:01:30 +0200

    replaced tabs with spaces

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -18,48 +18,50 @@ ADMIN_PASSWORD="${ADMIN_PASSWORD:-admin123}"
 
 
 function check_db_init {
-	db_rc=1
-	table_num=""
-	while [ $db_rc -ne 0 ]; do	
-		echo "Try to check DB state ..."
-		table_num=$(echo "SELECT COUNT(*) FROM information_schema.tables WHERE TABLE_SCHEMA ='passwordmanager';" |  mysql -h $DB_HOST -u $DB_USER --password="$DB_PASSWORD" $DB_DATABASE 2>/dev/null | tail -n 1)
-		db_rc=$?
+    db_rc=1
+    table_num=""
+    while [ $db_rc -ne 0 ]; do    
+        echo "Try to check DB state ..."
+        table_num=$(echo "SELECT COUNT(*) FROM information_schema.tables WHERE TABLE_SCHEMA ='passwordmanager';" |  mysql -h $DB_HOST -u $DB_USER --password="$DB_PASSWORD" $DB_DATABASE 2>/dev/null | tail -n 1)
+        db_rc=$?
         if [ ! -z "$table_num" ] && [ "$db_rc" -eq "0" ]; then
-        	break
+            break
         fi
-		echo "Could not connect to DB server. Try again in 1 second."
-		sleep 1
-	done
-	echo "Found $table_num table(s) in database."
+        echo "Could not connect to DB server. Try again in 1 second."
+        sleep 1
+    done
+    if [ ! -z "$table_num" ];
+        echo "Found $table_num table(s) in database."
+    fi
 
     if [ ! -z "$table_num" ] && [ "$table_num" -ne "0" ]; then
-    	echo "Skip database initialization."
-    	IS_DATABASE_INITIALIZED=true    	
+        echo "Skip database initialization."
+        IS_DATABASE_INITIALIZED=true        
     fi
 }
 
 
 function run_init {
-	echo "Run DB initialization"
-	composer artisan-cache
-	php artisan migrate --force
-	php artisan db:seed --class=Database\\Seeders\\Database
-	php artisan user:create --email=$ADMIN_MAIL --name=$ADMIN_USER --password=$ADMIN_PASSWORD --admin
+    echo "Run DB initialization"
+    composer artisan-cache
+    php artisan migrate --force
+    php artisan db:seed --class=Database\\Seeders\\Database
+    php artisan user:create --email=$ADMIN_MAIL --name=$ADMIN_USER --password=$ADMIN_PASSWORD --admin
 }
 
 
 function run_webserver {
-	php-fpm -D
-	nginx -g 'daemon off;'
+    php-fpm -D
+    nginx -g 'daemon off;'
 }
 
 
 function update_env {
-	echo "Update env file $ENV_FILE"
-	echo "DB_HOST=$DB_HOST"         >> $ENV_FILE
-	echo "DB_DATABASE=$DB_DATABASE" >> $ENV_FILE
-	echo "DB_USERNAME=$DB_USER"     >> $ENV_FILE
-	echo "DB_PASSWORD=$DB_PASSWORD" >> $ENV_FILE
+    echo "Update env file $ENV_FILE"
+    echo "DB_HOST=$DB_HOST"         >> $ENV_FILE
+    echo "DB_DATABASE=$DB_DATABASE" >> $ENV_FILE
+    echo "DB_USERNAME=$DB_USER"     >> $ENV_FILE
+    echo "DB_PASSWORD=$DB_PASSWORD" >> $ENV_FILE
 }
 
 
@@ -70,7 +72,7 @@ composer artisan-cache
 
 check_db_init
 if [ "$IS_DATABASE_INITIALIZED" = "false" ]; then
-	run_init
+    run_init
 fi
 
 run_webserver
