8b3852ce3c6376d50cfbaac874caa49d19095ba5
Lito <lito@eordes.com>
2022-09-07 00:24:09 +0200

    Updated docker

--- docker/init.sh
+++ docker/init.sh
@@ -1,4 +1,8 @@
-composer install --no-dev --optimize-autoloader --classmap-authoritative
+#!/bin/bash
+
+if [ "$APP_KEY" == "base64:YDd7vBg1pOO9e44ROzZT9MUkfB4p6aKBswlyuNJrxQo=" ]; then
+    echo -e "\e[41m WARNING: YOU ARE USING THE DEFAULT APP_KEY VALUE. PLEASE UPDATE THIS KEY ON FILE docker/.env \e[0m"
+fi
 
 php artisan migrate --force
 php artisan db:seed --force --class=Database\\Seeders\\Database
