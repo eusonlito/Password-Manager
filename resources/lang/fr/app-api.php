d0616f1e916d4acb671f9b5c630bd844908eda88
wangee <christian@vanguers.be>
2022-10-21 12:39:47 +0200

    Create app-api.php
    First draft of French translation

---
+++ resources/lang/fr/app-api.php
@@ -0,0 +1,7 @@
+<?php return [
+    'error' => [
+        'decrypt' => 'Le contenu de l\'Application n\'a pas pu être décrypté.',
+        'host-invalid' => 'Les résultats de cette page n\'ont pu être accédés.',
+        'not-found' => 'L\'Application demandée n\'est pour le moment pas disponible.',
+    ],
+];
