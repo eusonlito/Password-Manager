ab42004746bc4773eba7581c71e7b25681d65aa3
Andrea Bergamasco <a.bergamasco@groupecomplus.com>
2022-11-29 19:04:43 +0100

    Hide ':' in server listing when port is empty

--- resources/views/domains/app/types/server/list.blade.php
+++ resources/views/domains/app/types/server/list.blade.php
@@ -1,6 +1,6 @@
 <td>
     <a href="javascript:;" class="tooltip" title="{{ __('common.copy') }}" data-tippy-content="{{ __('common.copied') }}" data-copy>{{ ($row->payload('host')) }}</a>
-    :
+    @if($row->payload('port')):@endif
     <a href="javascript:;" class="tooltip" title="{{ __('common.copy') }}" data-tippy-content="{{ __('common.copied') }}" data-copy-ajax="{{ route('app.payload.key', [$row->id, 'port']) }}">@hidden($row->payload('port'))</a>
 </td>
 <td>
